4bb1118c6f29f2e9d844e871e7bf3271547530de
Sibel Dogru <sibeldogru@gmail.com>
2026-01-22 18:34:39 +0300

    "borrowerMail added to BookBorrowingRequest dto"

--- src/main/java/com/alisimsek/LibraryManagementProject/dto/request/BookBorrowingUpdateRequest.java
+++ src/main/java/com/alisimsek/LibraryManagementProject/dto/request/BookBorrowingUpdateRequest.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 public class BookBorrowingUpdateRequest {
 
     private String borrowerName;
+    private String borrowerMail;
     private LocalDate borrowingDate;
     private LocalDate returnDate;
 
